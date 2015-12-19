afd6c4ecff1e461ab0257b187f61ff0951c082ba
Yuki IGARASHI <me@bonprosoft.com>
2015-12-19 17:32:59 +0900

    Add ApplicationInsight to monitor server status.

---
+++ Controllers/AiHandleErrorAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
+
+namespace Server.Controllers
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class AiHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)
+            {
+                //If customError is Off, then AI HTTPModule will report the exception
+                if (filterContext.HttpContext.IsCustomErrorEnabled)
+                {
+                    var ai = new TelemetryClient();
+                    ai.TrackException(filterContext.Exception);
+                }
+            }
+            base.OnException(filterContext);
+        }
+    }
+}
